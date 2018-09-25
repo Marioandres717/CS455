7cad2564c504b72d73821e382417f3dcff839d0c
Mario Rendon Zapata <rendon2m@udml5.local>
2018-09-25 12:57:15 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Mario Rendon Zapata on 2018-09-25.
+//  Copyright © 2018 Mario Rendon Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
