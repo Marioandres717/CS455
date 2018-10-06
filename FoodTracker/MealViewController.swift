ed8206a49097efebfab46025a112630007a7dc5d
Mario Rendon Zapata <rendon2m@udml17.cs.uregina.ca>
2018-10-05 20:30:45 -0600

    Implement Edit and Delete Behaviour

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -28,6 +28,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Handle the text field's user input through delegate callbacks
         nameTextField.delegate = self
         
+        // Set up views if editing an existing meal
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the save button only if the text field has a valid Meal name
         updateSaveButtonState()
     }
@@ -72,8 +80,19 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     // MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        // Dismisses the modal scene and animates the transition back to the previous scene (Meal list)
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push Presentation), ths view controller needs to be dismissed in two different ways
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            // Dismisses the modal scene and animates the transition back to the previous scene (Meal list)
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            // If the user click an existing meal for editing
+            // pop view removes the view from the stack navigation and properly animates this
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     // This method lets you configure a view controller before it's presented
